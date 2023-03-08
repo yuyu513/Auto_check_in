6655968383119f7d757ca17743f70030a24b04d0
yuyu <44204450+yuyu513@users.noreply.github.com>
2023-03-08 23:14:30 +0800

    Update Auto_Account.py

--- Auto_Account.py
+++ Auto_Account.py
@@ -1,5 +1,5 @@
-Username = 'yuyu513'
-Password = 'Yu513302-'
-bot_token = "bot788409798:AAFPDKZoQTDYzzteecs6KoDWHIIsC1c75z8"
-chat_id = "636822635"
-api_key = 'e7d9cac87edd5e6d5766296bde05ab9b'
+Username = ''
+Password = ''
+bot_token = ''
+chat_id = ''
+api_key = ''
